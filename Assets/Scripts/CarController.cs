0d62a9bccb2540694dceae117ad0c96cf9c97d80
Honodera1224 <H.onodera.gc25@morijyobi.ac.jp>
2025-07-26 09:30:36 +0900

    変更

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -2,60 +2,57 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
+    //GameObject flag; //※旗のデータを入れる変数(箱のようなもの)
 
-    //GameObject flag;　※旗のデータを入れる変数(箱のようなもの)
+    //public float speed = 0f; //※初速度を入れる変数(箱のようなもの)
 
-    //float speed = 0f;  ※初速度を入れる変数(箱のようなもの)
-
-    //Vector2 startPos;　　※車がいる位置を入れる変数(箱のようなもの)
-
-    //void Start()  ※ゲームが始まったときに実行されるもの、{}の内容が実行される
-
-    //{
-
-    //Application.targetFrameRate = 60;  ※ゲームのフレームレートを60に固定にする設定
-
-    //this.flag = GameObject.Find("flag_0");　※旗の変数に旗のデータを見つけてきて入れてあげる作業
-
-    //}
+    //Vector2 startPos; //※車がいる位置を入れる変数(箱のようなもの)
 
 
-    //void Update() 　※毎フレーム実行されるもの、{}の内容が実行される
-    //{
+    //public float swipeLength; //距離を求める
 
-    //※if()は、もし～の時に実行するという意味、その後に{}の内容が実行される
-    //if (Input.GetMouseButtonDown(0))  ※もし左クリックが押された時
-    //   {
+    void Start()
+    {
+        //Application.targetFrameRate = 60; //※ゲームのフレームレートを60に固定にする設定
 
-    //    this.startPos = Input.mousePosition;  ※クリックした時の座標をstartPosに入れる
+        //this.flag = GameObject.Find("flag"); //※旗の変数に旗のデータを見つけてきて入れてあげる作業
+    }
 
-    //    }
+    void Update()
+    {
 
-    //else if (Input.GetMouseButtonUp(0))  ※もし左クリックが離された時
-    //   {
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    this.startPos = Input.mousePosition; // ※クリックをした時の座標をstartPosに入れる
+        //}
+        //else if (Input.GetMouseButtonUp(0))
+        //{
+
+        //    Vector2 endPos = Input.mousePosition; // ※クリックを離した時の座標をendPosに入れる
+
+        //    swipeLength = endPos.x - startPos.x; // ※クリックを離した時の座標 - クリックした時の座標
+
+        //    float length = flag.transform.position.x - transform.position.x; // ※旗と車の位置を引いて、距離を求めて入れる
+
+        //    if (swipeLength < 0 || length < 0 || GameDirector.clear)
+        //    {
+        //        return; //  ※もし、車が旗を越している、または後ろにスワイプした場合は、処理をここでストップ
+        //    }
+
+        //    speed = swipeLength / 1500.0f; // ※スワイプの長さ↑を初速度に変換する
+
+        //    GetComponent<AudioSource>().Play(); //※効果音再生
+        //}
 
 
-    //    Vector2 endPos = Input.mousePosition;  ※クリックを離した時の座標をendPosに入れる
+        //transform.Translate(speed, speed, 0); // ※Xにspeedの値分、移動する
 
-    //    float swipeLength = endPos.x - this.startPos.x; 　※クリックを離した時の座標 - クリックした時の座標
+        //if (speed <= 0.05)
+        //{
+        //    speed = 0f;
+        //}
 
-    //    float length = flag.transform.position.x - transform.position.x;  ※旗と車の位置を引いて、距離を求めて入れる
-
-    //    if(swipeLength < 0 || length < 0)  ※もし、車が旗を越している、または後ろにスワイプした場合は、処理をここでストップ
-    //       {
-
-    //           return;
-
-    //       }
-
-    //    this.speed = swipeLength / 1500.0f;  ※スワイプの長さ↑を初速度に変換する
-
-    //    GetComponent<AudioSource>().Play();　　※効果音再生
-
-    //    }
-
-    //transform.Translate(speed,0,0);　※Xにspeedの値分、移動する
-
-    //speed *= 0.98f;  ※初速度に0.98をかけることで、だんだんと減速させる
-    //}
+        //speed *= 0.98f; // ※初速度に0.98をかけることで、だんだんと減速させる
+    }
 }
+
---
+++ Assets/Scripts/ToriSpriteController.cs
@@ -0,0 +1,109 @@
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class ToriSpriteController : MonoBehaviour
+{
+    AsyncOperationHandle<Sprite> Handle;
+    Sprite[] Tatisprite = new Sprite[3];
+    Sprite[] Tobisprite = new Sprite[3];
+    float fps = 0.05f;
+    SpriteRenderer Renderer;
+    CarController Car;
+    Rigidbody2D rb;
+    float tatiTime = 0;
+    float tobuTime = 0;
+    int tatiIdx = 1;
+    int tobiIdx = 1;
+    int tatinum = 1;
+    int tobinum = 1;
+    static public int count = 0;
+    static public bool fly;
+
+    void Start()
+    {
+        Set();
+        Renderer = GetComponent<SpriteRenderer>();
+        Car = GetComponent<CarController>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Set()
+    {
+        Addressables.LoadAssetAsync<Sprite>("taiki01").Completed += sprite =>
+        {
+            Tatisprite[0] = sprite.Result;
+        };
+        Addressables.LoadAssetAsync<Sprite>("taiki02").Completed += sprite =>
+        {
+            Tatisprite[1] = sprite.Result;
+        };
+        Addressables.LoadAssetAsync<Sprite>("taiki03").Completed += sprite =>
+        {
+            Tatisprite[2] = sprite.Result;
+        };
+
+        Addressables.LoadAssetAsync<Sprite>("jump01").Completed += sprite =>
+        {
+            Tobisprite[0] = sprite.Result;
+        };
+        Addressables.LoadAssetAsync<Sprite>("jump02").Completed += sprite =>
+        {
+            Tobisprite[1] = sprite.Result;
+        };
+        Addressables.LoadAssetAsync<Sprite>("jump03").Completed += sprite =>
+        {
+            Tobisprite[2] = sprite.Result;
+        };
+
+    }
+    void Update()
+    {
+        SpAnimation();
+    }
+
+    void SpAnimation()
+    {
+        if (!fly)
+        {
+            tatiTime += Time.deltaTime;
+            if (tatiTime >= fps)
+            {
+                tatiTime = 0;
+                if (tatiIdx == Tatisprite.Length - 1)
+                    tatinum = -1;
+                else if (tatiIdx == 0)
+                    tatinum = 1;
+                tatiIdx += tatinum;
+            }
+            Renderer.sprite = Tatisprite[tatiIdx];
+        }
+        else if (fly)
+        {
+            tobuTime += Time.deltaTime;
+            if (tobuTime >= fps)
+            {
+                tobuTime = 0;
+                if (tobiIdx == Tobisprite.Length - 1)
+                    tobinum = -1;
+                else if (tobiIdx == 0)
+                    tobinum = 1;
+                tobiIdx += tobinum;
+            }
+            Renderer.sprite = Tobisprite[tobiIdx];
+
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        fly = false;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        fly = true;
+        count++;
+    }
+
+}

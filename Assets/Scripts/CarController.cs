68867108318b289efd40a7ad0aa90d787688dccd
MorijyobiDesktopPC1 <example@morijyobi.ac.jp>
2026-05-02 10:44:28 +0900

    修正

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,57 +1,55 @@
-//using UnityEngine;
+using UnityEngine;
 
-//public class CarController : MonoBehaviour
-//{
-//    GameObject flag; //※旗のデータを入れる変数(箱のようなもの)
+public class CarController : MonoBehaviour
+{
+    //GameObject flag; //※旗のデータを入れる変数(箱のようなもの)
 
-//    public float speed = 0f; //※初速度を入れる変数(箱のようなもの)
+    //public float speed = 0f; //※初速度を入れる変数(箱のようなもの)
 
-//    Vector2 startPos; //※鳥がいる位置を入れる変数(箱のようなもの)
+    //Vector2 startPos; //※鳥がいる位置を入れる変数(箱のようなもの)
 
-//    public float swipeLength; //距離を求める
+    //public float swipeLength; //距離を求める
 
-//    void Start()
-//    {
-//        Application.targetFrameRate = 60; //※ゲームのフレームレートを60に固定にする設定
+    //void Start()
+    //{
+    //    Application.targetFrameRate = 60; //※ゲームのフレームレートを60に固定にする設定
 
-//        this.flag = GameObject.Find("flag"); //※旗の変数に旗のデータを見つけてきて入れてあげる作業
-//    }
+    //    this.flag = GameObject.Find("flag"); //※旗の変数に旗のデータを見つけてきて入れてあげる作業
+    //}
 
-//    void Update()
-//    {
+    //void Update()
+    //{
 
-//        if (Input.GetMouseButtonDown(0))
-//        {
-//            this.startPos = Input.mousePosition; // ※クリックをした時の座標をstartPosに入れる
-//        }
-//        else if (Input.GetMouseButtonUp(0))
-//        {
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        this.startPos = Input.mousePosition; // ※クリックをした時の座標をstartPosに入れる
+    //    }
+    //    else if (Input.GetMouseButtonUp(0))
+    //    {
 
-//            Vector2 endPos = Input.mousePosition; // ※クリックを離した時の座標をendPosに入れる
+    //        Vector2 endPos = Input.mousePosition; // ※クリックを離した時の座標をendPosに入れる
 
-//            swipeLength = endPos.x - startPos.x; // ※クリックを離した時の座標 - クリックした時の座標
+    //        swipeLength = endPos.x - startPos.x; // ※クリックを離した時の座標 - クリックした時の座標
 
-//            float length = flag.transform.position.x - transform.position.x; // ※旗と車の位置を引いて、距離を求めて入れる
+    //        if (swipeLength < 0 || GameDirector.clear || ToriSpriteController.fly)
+    //        {
+    //            return; //  ※もし、車が旗を越している、または後ろにスワイプした場合は、処理をここでストップ
+    //        }
 
-//            if (swipeLength < 0 || GameDirector.clear || ToriSpriteController.fly)
-//            {
-//                return; //  ※もし、車が旗を越している、または後ろにスワイプした場合は、処理をここでストップ
-//            }
+    //        speed = swipeLength / 1500.0f; // ※スワイプの長さ↑を初速度に変換する
 
-//            speed = swipeLength / 1500.0f; // ※スワイプの長さ↑を初速度に変換する
-
-//            GetComponent<AudioSource>().Play(); //※効果音再生
-//        }
+    //        GetComponent<AudioSource>().Play(); //※効果音再生
+    //    }
 
 
-//        transform.Translate(speed, speed, 0); // ※Xにspeedの値分、移動する
+    //    transform.Translate(speed, speed, 0); // ※Xにspeedの値分、移動する
 
-//        if (speed <= 0.05)
-//        {
-//            speed = 0f;
-//        }
+    //    if (speed <= 0.05)
+    //    {
+    //        speed = 0f;
+    //    }
 
-//        speed *= 0.98f; // ※初速度に0.98をかけることで、だんだんと減速させる
-//    }
-//}
+    //    speed *= 0.98f; // ※初速度に0.98をかけることで、だんだんと減速させる
+    //}
+}
 
--- Assets/Scripts/ToriSpriteController.cs
+++ Assets/Scripts/ToriSpriteController.cs
@@ -1,111 +1,77 @@
-//using UnityEngine;
-//using UnityEngine.AddressableAssets;
-//using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
-//public class ToriSpriteController : MonoBehaviour
-//{
-//    AsyncOperationHandle<Sprite> Handle;
-//    Sprite[] Tatisprite = new Sprite[3];
-//    Sprite[] Tobisprite = new Sprite[3];
-//    float fps = 0.05f;
-//    SpriteRenderer Renderer;
-//    CarController Car;
-//    Rigidbody2D rb;
-//    float tatiTime = 0;
-//    float tobuTime = 0;
-//    int tatiIdx = 1;
-//    int tobiIdx = 1;
-//    int tatinum = 1;
-//    int tobinum = 1;
-//    public int count = 3;
-//    static public bool fly;
+public class ToriSpriteController : MonoBehaviour
+{
+    AsyncOperationHandle<Sprite> Handle;
+   
+    [SerializeField] Sprite[] Tatisprite = new Sprite[3];
+    [SerializeField] Sprite[] Tobisprite = new Sprite[3];
+    float fps = 0.05f;
+    SpriteRenderer Renderer;
+    float tatiTime = 0;
+    float tobuTime = 0;
+    int tatiIdx = 1;
+    int tobiIdx = 1;
+    int tatinum = 1;
+    int tobinum = 1;
+    public int count = 3;
+    static public bool fly;
 
-//    void Start()
-//    {
-//        Set();
-//        Renderer = GetComponent<SpriteRenderer>();
-//        Car = GetComponent<CarController>();
-//        rb = GetComponent<Rigidbody2D>();
-//    }
+    void Start()
+    {
 
-//    void Set()
-//    {
-        
+        Renderer = GetComponent<SpriteRenderer>();
+    }
+ 
+    void Update()
+    {
+        SpAnimation();
+    }
 
-//        Addressables.LoadAssetAsync<Sprite>("taiki01").Completed += sprite =>
-//        {
-//            Tatisprite[0] = sprite.Result;
-//        };
-//        Addressables.LoadAssetAsync<Sprite>("taiki02").Completed += sprite =>
-//        {
-//            Tatisprite[1] = sprite.Result;
-//        };
-//        Addressables.LoadAssetAsync<Sprite>("taiki03").Completed += sprite =>
-//        {
-//            Tatisprite[2] = sprite.Result;
-//        };
+    void SpAnimation()
+    {
+        if (!fly)
+        {
+            tatiTime += Time.deltaTime;
+            if (tatiTime >= fps)
+            {
+                tatiTime = 0;
+                if (tatiIdx == Tatisprite.Length - 1)
+                    tatinum = -1;
+                else if (tatiIdx == 0)
+                    tatinum = 1;
+                tatiIdx += tatinum;
+            }
+            Renderer.sprite = Tatisprite[tatiIdx];
+        }
+        else if (fly)
+        {
+            tobuTime += Time.deltaTime;
+            if (tobuTime >= fps)
+            {
+                tobuTime = 0;
+                if (tobiIdx == Tobisprite.Length - 1)
+                    tobinum = -1;
+                else if (tobiIdx == 0)
+                    tobinum = 1;
+                tobiIdx += tobinum;
+            }
+            Renderer.sprite = Tobisprite[tobiIdx];
 
-//        Addressables.LoadAssetAsync<Sprite>("jump01").Completed += sprite =>
-//        {
-//            Tobisprite[0] = sprite.Result;
-//        };
-//        Addressables.LoadAssetAsync<Sprite>("jump02").Completed += sprite =>
-//        {
-//            Tobisprite[1] = sprite.Result;
-//        };
-//        Addressables.LoadAssetAsync<Sprite>("jump03").Completed += sprite =>
-//        {
-//            Tobisprite[2] = sprite.Result;
-//        };
+        }
+    }
 
-//    }
-//    void Update()
-//    {
-//        SpAnimation();
-//    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
 
-//    void SpAnimation()
-//    {
-//        if (!fly)
-//        {
-//            tatiTime += Time.deltaTime;
-//            if (tatiTime >= fps)
-//            {
-//                tatiTime = 0;
-//                if (tatiIdx == Tatisprite.Length - 1)
-//                    tatinum = -1;
-//                else if (tatiIdx == 0)
-//                    tatinum = 1;
-//                tatiIdx += tatinum;
-//            }
-//            Renderer.sprite = Tatisprite[tatiIdx];
-//        }
-//        else if (fly)
-//        {
-//            tobuTime += Time.deltaTime;
-//            if (tobuTime >= fps)
-//            {
-//                tobuTime = 0;
-//                if (tobiIdx == Tobisprite.Length - 1)
-//                    tobinum = -1;
-//                else if (tobiIdx == 0)
-//                    tobinum = 1;
-//                tobiIdx += tobinum;
-//            }
-//            Renderer.sprite = Tobisprite[tobiIdx];
+        fly = false;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        fly = true;
+        count--;
+    }
 
-//        }
-//    }
-
-//    private void OnCollisionEnter2D(Collision2D collision)
-//    {
-
-//        fly = false;
-//    }
-//    private void OnCollisionExit2D(Collision2D collision)
-//    {
-//        fly = true;
-//        count--;
-//    }
-
-//}
+}
